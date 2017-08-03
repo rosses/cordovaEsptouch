50fab7262dc4ea16a9f8b83125cd4e38241b81b7
Roberto Osses <rosses@gmail.com>
2017-08-03 00:59:56 -0400

    new ios

--- src/ios/esptouchPlugin.m
+++ src/ios/esptouchPlugin.m
@@ -28,12 +28,11 @@
     NSString *apBssid = (NSString *)[command.arguments objectAtIndex:1];
     NSString *apPwd = (NSString *)[command.arguments objectAtIndex:2];
     NSString *isSsidHiddenStr=(NSString *)[command.arguments objectAtIndex:3];
-    
     BOOL isSsidHidden = true;
     if([isSsidHiddenStr compare:@"NO"]==NSOrderedSame){
         isSsidHidden=false;
     }
-    int taskCount = (int)[command.arguments objectAtIndex:4];
+    int taskCount = (int)[[command.arguments objectAtIndex:4] intValue];
     self._esptouchTask =
     [[ESPTouchTask alloc]initWithApSsid:apSsid andApBssid:apBssid andApPwd:apPwd andIsSsidHiden:isSsidHidden];
     EspTouchDelegateImpl *esptouchDelegate=[[EspTouchDelegateImpl alloc]init];
@@ -41,57 +40,54 @@
     esptouchDelegate.commandDelegate=self.commandDelegate;
     [self._esptouchTask setEsptouchDelegate:esptouchDelegate];
     [self._condition unlock];
-    NSArray * esptouchResultArray = [self._esptouchTask executeForResults:taskCount];
     [self.commandDelegate runInBackground:^{
     dispatch_queue_t  queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     dispatch_async(queue, ^{
-        // show the result to the user in UI Main Thread
-        dispatch_async(dispatch_get_main_queue(), ^{
-
-            
+        NSArray * esptouchResultArray = [self._esptouchTask executeForResults:taskCount];
+       // show the result to the user in UI Main Thread
+        NSString *daStr = @"imlink";
+        const char *queueName = [daStr UTF8String];
+        dispatch_queue_t myQueue = dispatch_queue_create(queueName, DISPATCH_QUEUE_CONCURRENT);
+        dispatch_async(myQueue, ^{
             ESPTouchResult *firstResult = [esptouchResultArray objectAtIndex:0];
             // check whether the task is cancelled and no results received
             if (!firstResult.isCancelled)
             {
-                //NSMutableString *mutableStr = [[NSMutableString alloc]init];
-                //NSUInteger count = 0;
+                NSMutableString *mutableStr = [[NSMutableString alloc]init];
+                NSUInteger count = 0;
                 // max results to be displayed, if it is more than maxDisplayCount,
                 // just show the count of redundant ones
-                //const int maxDisplayCount = 5;
+                const int maxDisplayCount = 5;
                 if ([firstResult isSuc])
                 {
-                    
-//                    for (int i = 0; i < [esptouchResultArray count]; ++i)
-//                    {
-//                        ESPTouchResult *resultInArray = [esptouchResultArray objectAtIndex:i];
-//                        [mutableStr appendString:[resultInArray description]];
-//                        [mutableStr appendString:@"\n"];
-//                        count++;
-//                        if (count >= maxDisplayCount)
-//                        {
-//                            break;
-//                        }
-//                    }
-//                    
-//                    if (count < [esptouchResultArray count])
-//                    {
-//                        [mutableStr appendString:[NSString stringWithFormat:@"\nthere's %lu more result(s) without showing\n",(unsigned long)([esptouchResultArray count] - count)]];
-//                    }
+                    for (int i = 0; i < [esptouchResultArray count]; ++i)
+                    {
+                        ESPTouchResult *resultInArray = [esptouchResultArray objectAtIndex:i];
+                        [mutableStr appendString:[resultInArray description]];
+                        [mutableStr appendString:@"\n"];
+                        count++;
+                        if (count >= maxDisplayCount)
+                        {
+                            break;
+                        }
+                    }
+                    if (count < [esptouchResultArray count])
+                    {
+                        [mutableStr appendString:[NSString stringWithFormat:@"\nthere's %lu more result(s) without showing\n",(unsigned long)([esptouchResultArray count] - count)]];
+                    }
                     CDVPluginResult* pluginResult = nil;
                     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: @"finished"];
                     [pluginResult setKeepCallbackAsBool:true];
                     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
                 }
-                
                 else
                 {
                     CDVPluginResult* pluginResult = nil;
-                    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: @"Esptouch fail"];
+                    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString: @"Esptouch fail"];
                     [pluginResult setKeepCallbackAsBool:true];
                     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
                 }
             }
-            
         });
     });
     }];
