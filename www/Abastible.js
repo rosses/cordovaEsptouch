4a7ca1617d35c449b287fe6e64ed6fdb5436429a
Roberto Osses <rosses@gmail.com>
2017-08-06 02:24:48 -0400

    Fiix

--- www/Abastible.js
+++
@@ -1,5 +0,0 @@
-var exec = require('cordova/exec');
-
-exports.startMeasurement = function(weight, tarra, type, id, full, success, error) {
-    exec(success, error, "Abastible", "startMeasurement", [weight, tarra, type, id, full]);
-};
